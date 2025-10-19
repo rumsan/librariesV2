297b4fe90c00b616ddcea1fd6f24e66a4d3d9b32
Santosh Shrestha <santosh@rumsan.com>
2025-10-19 19:30:00 +0545

    cleanup

--- apps/api/src/source-data/source-data.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SourceDataService } from './source-data.service';
-import { SourceDataController } from './source-data.controller';
-
-@Module({
-  controllers: [SourceDataController],
-  providers: [SourceDataService],
-})
-export class SourceDataModule {}
